811315f433dc21f68b1951a29103212d66d52faf
amrita-arun <amrita.arun2013@gmail.com>
2024-06-27 00:48:31 -0700

    base project - no directories

---
+++ InfluencerConnect/InfluencerConnect/InfluencerConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfluencerConnectApp.swift
+//  InfluencerConnect
+//
+//  Created by Administrator on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct InfluencerConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
